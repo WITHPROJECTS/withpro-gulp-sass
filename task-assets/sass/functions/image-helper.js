67c7ccf94fc42dbd452c0169cda16a336c46dc52
yokotak0527 <mail@yokotakenji.me>
2016-12-27 17:14:30 +0900

    fix

--- task-assets/sass/functions/image-helper.js
+++ task-assets/sass/functions/image-helper.js
@@ -22,12 +22,12 @@ module.exports = (param)=>{
         'image-width($input: "")' : (input)=>{
             let getInputVal = input.getValue();
             let filePath    = path.join(rtvPath.image, getInputVal);
-            return new sassType.Number(sizeOf(filePath).width, 'px');
+            return new sassType.Number(sizeOf(filePath).width, 'px', 'px');
         },
         'image-height($input: "")' : (input)=>{
             let getInputVal = input.getValue();
             let filePath    = path.join(rtvPath.image, getInputVal);
-            return new sassType.Number(sizeOf(filePath).height, 'px')
+            return new sassType.Number(sizeOf(filePath).height, 'px', 'px')
         }
     }
 }
